8c80ccf98a67054ccce57d56215b54d90523e08d
WChan <baewc95@gmail.com>
2023-04-24 11:34:15 +0900

    [FE]feat : 초기 폴더 생성

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserMainPage from "./pages/UserMainPage/UserMainPage";
+import MyGarden from "./pages/MyGarden/MyGarden";
+import Landing from "./pages/Landing/Landing";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/usermain" element={<UserMainPage />} />
+        <Route path="/flowery" element={<Landing />} />
+        <Route path="/flowery/usermain" element={<UserMainPage />} />
+        <Route path="/flowery/mygarden" element={<MyGarden />} />
       </Routes>
     </BrowserRouter>
   );
