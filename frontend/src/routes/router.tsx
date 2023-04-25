43aa34c492e9b76c996ff0875b1a29773c28f35b
changgohap <changgohap@gmail.com>
2023-04-25 15:54:19 +0900

    [FE]feat/UserReservation-ccg : 구매자 메인페이지 구성중

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,25 +1,27 @@
-import { createBrowserRouter } from 'react-router-dom';
-import UserMainLayout from '../layouts/UserMainLayout';
-import UserMainPage from '../pages/UserMainPage/UserMainPage';
-import SellerMainPage from '../pages/SellerMainPage/SellerMainPage';
-import MyGarden from '../pages/MyGarden/MyGarden';
-import SellerMainLayout from '../layouts/SellerMainLayout';
+import { createBrowserRouter } from "react-router-dom";
+import UserMainLayout from "../layouts/UserMainLayout";
+import UserMainPage from "../pages/UserMainPage/UserMainPage";
+import SellerMainPage from "../pages/SellerMainPage/SellerMainPage";
+import MyGarden from "../pages/MyGarden/MyGarden";
+import SellerMainLayout from "../layouts/SellerMainLayout";
+import Reservation from "../pages/Reservation/Reservation";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <UserMainLayout />,
     // errorElement: <NotFound />, // 라우터에 없는 경로로 이동시 NotFound 컴포넌트 화면에 띄운다.
     children: [
-      { path: '/', element: <UserMainPage /> },
-      { path: '/mygarden', element: <MyGarden /> },
+      { path: "/", element: <UserMainPage /> },
+      { path: "/reservation", element: <Reservation /> },
+      { path: "/mygarden", element: <MyGarden /> },
     ],
   },
   {
-    path: '/',
+    path: "/",
     element: <SellerMainLayout />,
     // errorElement: <NotFound />, // 라우터에 없는 경로로 이동시 NotFound 컴포넌트 화면에 띄운다.
-    children: [{ path: '/seller', element: <SellerMainPage /> }],
+    children: [{ path: "/seller", element: <SellerMainPage /> }],
   },
 ]);
 
