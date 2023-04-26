15548ad4493dbfc4810c04a13995941513aac660
이승민 <curiously12@naver.com>
2023-04-26 15:51:18 +0900

    [FE]feat/seller : 메인페이지 틀 완성

--- frontend/src/components/Seller/PrintTitle.tsx
+++ frontend/src/components/Seller/PrintTitle.tsx
@@ -2,5 +2,10 @@ import React from "react";
 import styles from "./PrintTitle.module.scss";
 
 export default function PrintTitle() {
-  return <div className={styles.font}>출력 내역</div>;
+  return (
+    <main>
+      <div className={styles.font1}>예약 내역</div>
+      <div className={styles.font2}>오늘 총 N건의 예약이 있습니다. </div>
+    </main>
+  );
 }
--- frontend/src/pages/SellerMainPage/SellerMainPage.tsx
+++ frontend/src/pages/SellerMainPage/SellerMainPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ManagePrint from "../../components/Seller/ManagePrint";
+import MainAnalyzing from "../../components/Seller/MainAnalyzing";
 
 export default function SellerMainPage() {
   return (
     <div>
       <ManagePrint />
+      <MainAnalyzing />
     </div>
   );
 }
