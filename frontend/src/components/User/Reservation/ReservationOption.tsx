8d4eca1e92f91a736b9bb395841491b2b31dca58
changgohap <changgohap@gmail.com>
2023-05-01 01:57:30 +0900

    [FE]feature/reservationlage[인포 레이아웃 (완) & 시간(???), 날짜 예약 기능(datepicker) (구현중) ]-ccg

--- frontend/src/components/User/Reservation/ReservationOption.tsx
+++ frontend/src/components/User/Reservation/ReservationOption.tsx
@@ -1,24 +1,16 @@
 import React from "react";
 import Timeselect from "./TImeSelect";
 import Dayselect from "./DaySelect";
+import OptionHeader from "./OptionHeader";
+import ShopInfo from "./ShopInfo";
+
 export default function ReservationOption() {
-  const positions = [
-    {
-      content: "꽃들 info",
-      title: "꽃들",
-      latlng: { lat: 35.1569, lng: 129.0591 },
-    },
-    {
-      content: "써니플레르 info",
-      title: "써니플레르",
-      latlng: { lat: 35.313, lng: 129.0103 },
-    },
-  ];
   return (
-    <>
-      <p>info</p>
+    <div className="flex flex-col">
+      <OptionHeader />
+      <ShopInfo />
       <Dayselect />
       <Timeselect />
-    </>
+    </div>
   );
 }
