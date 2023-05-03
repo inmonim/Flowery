7e05a0289040427e3311eff76e307ce9bd100d58
changgohap <changgohap@gmail.com>
2023-05-04 02:23:34 +0900

    [FE]feature/releasepage: 시연intro 구현[완], 시연 카테고리 구현 [완], 시연 카테고리 중 letter 구현중 [50%]

---
+++ frontend/src/components/ReleasePage/ReleasePictures.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import floral from "../../assets/flowershop1.jpg";
+import memory1 from "../../assets/profile1.png";
+import memory2 from "../../assets/profile2.png";
+import memory3 from "../../assets/profile3.png";
+import memory4 from "../../assets/profile4.png";
+import memory5 from "../../assets/profile5.png";
+
+export default function ReleasePictures() {
+  const memories = [memory1, memory2, memory3, memory4, memory5];
+
+  return (
+    <div className="bg-[#FEF7F1]">
+      <div>
+        <img src={floral} alt="shop picture" />
+      </div>
+      <div className="bg-white h-44444444h border">
+        <p className="flex justify-center">our</p>
+        <p className="flex justify-center">memories</p>
+      </div>
+      <div className="pr-[5.33vw] pl-[5.33vw]">
+        {memories.map((memory, index) => (
+          <img src={memory} key={index} />
+        ))}
+      </div>
+    </div>
+  );
+}
--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -11,9 +11,13 @@ import SellerLoginPage from "../pages/SellerLoginPage/SellerLoginPage";
 import ReservationOption from "../components/User/Reservation/ReservationOption";
 import SignUpPage from "../pages/SignIn/SignUpPage";
 import NonMemberPage from "../pages/SignIn/NonMember";
-import LetterPage from "../pages/WritingPage/Letter";
+import LetterPage from "../pages/WritingPage/WritingPage";
 import ProtoPage from "../pages/SellerMainPage/SellerMainProto";
 import TestPage from "../pages/TestPage";
+import ReleaseIntro from "../components/ReleasePage/ReleaseIntro";
+import ReleaseCategory from "../components/ReleasePage/ReleaseCategory";
+import ReleaseLetters from "../components/ReleasePage/ReleaseLetters";
+import ReleasePictures from "../components/ReleasePage/ReleasePictures";
 
 const router = createBrowserRouter([
   {
@@ -40,6 +44,16 @@ const router = createBrowserRouter([
       { path: "/seller", element: <SellerMainPage /> },
       { path: "/seller/login", element: <SellerLoginPage /> },
       { path: "/seller/proto", element: <ProtoPage /> },
+    ],
+  },
+  {
+    // 시연용
+    path: "/",
+    children: [
+      { path: "/release", element: <ReleaseIntro /> },
+      { path: "/releasecategory", element: <ReleaseCategory /> },
+      { path: "/releasepictures", element: <ReleasePictures /> },
+      { path: "/releaseletter", element: <ReleaseLetters /> },
     ],
   },
 ]);
