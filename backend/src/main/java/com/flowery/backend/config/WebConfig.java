75765df41bd5b4735e329fa009aecdf32a04ea8c
GoldshireRobber <kamen12@naver.com>
2023-05-04 10:19:15 +0900

    feat: front 내부 nginx 수정

--- backend/src/main/java/com/flowery/backend/config/WebConfig.java
+++ backend/src/main/java/com/flowery/backend/config/WebConfig.java
@@ -1,13 +1,8 @@
 package com.flowery.backend.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -16,23 +11,5 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST");
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new ApiPrefixInterceptor()).addPathPatterns("/api/**");
-    }
-
-    private static class ApiPrefixInterceptor implements HandlerInterceptor {
-        @Override
-        public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-            String requestURI = request.getRequestURI();
-            if (!requestURI.startsWith("/api/")) {
-                String newURI = "/api" + requestURI;
-                request.getRequestDispatcher(newURI).forward(request, response);
-                return false;
-            }
-            return true;
-        }
     }
 }
