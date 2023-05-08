70e7dcaff912b0efed9b8e55041b0d52c289ef64
changgohap <changgohap@gmail.com>
2023-05-08 18:04:09 +0900

    [FE]feature/main에 링크 설정

--- frontend/src/pages/UserMainPage/UserMainPage.tsx
+++ frontend/src/pages/UserMainPage/UserMainPage.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function UserMainPage() {
   const navigate = useNavigate();
   function handleNavigate() {
     navigate("/writing");
   }
-  return <div onClick={handleNavigate}>이거본문임</div>;
+  return (
+    <div onClick={handleNavigate} className="flex flex-col">
+      <Link to="/reservation">예약 페이지입니다</Link>
+      <Link to="/release">qr 체크시 이동하는 페이지입니다</Link>
+      <Link to="/seller/proto">판매자 페이지입니다</Link>
+    </div>
+  );
 }
