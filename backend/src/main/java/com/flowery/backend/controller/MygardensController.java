c9a6c5b44ccaf89207663721c7633a8a506a9df0
YoungIl <octover1025@naver.com>
2023-05-10 00:55:33 +0900

    [BE]feat/myGarden : createMyGarden 해결 중

--- backend/src/main/java/com/flowery/backend/controller/MygardensController.java
+++ backend/src/main/java/com/flowery/backend/controller/MygardensController.java
@@ -1,13 +1,9 @@
 package com.flowery.backend.controller;
 
-import com.flowery.backend.model.dto.MessagesDto;
 import com.flowery.backend.model.dto.MygardensDto;
-import com.flowery.backend.model.entity.Messages;
 import com.flowery.backend.model.entity.Mygardens;
 import com.flowery.backend.sevice.MygardensService;
 import com.google.zxing.*;
-import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageConfig;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
@@ -16,13 +12,11 @@ import com.google.zxing.qrcode.QRCodeWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
@@ -48,6 +42,7 @@ public class MygardensController {
     
     @PostMapping
     public ResponseEntity<Mygardens> createMyGarden(@RequestBody MygardensDto mygardensDto) throws Exception {
+        LOGGER.info("createMyGarden가 호출되었습니다.");
         try {
             return new ResponseEntity<Mygardens>(mygardensService.createMyGarden(mygardensDto), HttpStatus.OK);
         } catch (Exception e) {
--- backend/src/main/java/com/flowery/backend/sevice/MygardensService.java
+++ backend/src/main/java/com/flowery/backend/sevice/MygardensService.java
@@ -1,6 +1,5 @@
 package com.flowery.backend.sevice;
 
-import com.flowery.backend.model.dto.MessagesDto;
 import com.flowery.backend.model.dto.MygardensDto;
 import com.flowery.backend.model.entity.Messages;
 import com.flowery.backend.model.entity.Mygardens;
@@ -8,13 +7,9 @@ import com.flowery.backend.model.entity.Users;
 import com.flowery.backend.repository.MessagesRepository;
 import com.flowery.backend.repository.MygardensRepository;
 import com.flowery.backend.repository.UsersRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class MygardensService {
@@ -75,11 +70,18 @@ public class MygardensService {
 
     public Mygardens createMyGarden(MygardensDto mygardensDto) {
         Mygardens mygarden = new Mygardens();
-
-        Messages message = messagesRepository.findByMessageId(mygardensDto.getMessageId());
+        System.out.println(mygardensDto.getMessageId().getClass().getName());
+        String messageId = mygardensDto.getMessageId();
+        System.out.println(messageId);
+        Messages message = messagesRepository.findByMessageId(messageId);
+        System.out.println(message);
+//        Messages message = messagesRepository.findById(messageId.toString()).get();
+//                .orElseThrow(() -> new NoSuchElementException("해당 messageId가 없습니다."));
+        System.out.println("여기까지 못 오는 거지?");
+        Integer userId = mygardensDto.getUserId();
+        Users user = usersRepository.findById(userId).orElseThrow(() -> new NoSuchElementException("해당 sample_id가 없습니다."));
         mygarden.setMessageId(message);
-        mygarden.setUserId(mygarden.getUserId());
-
+        mygarden.setUserId(user);
 
         return mygardensRepository.save(mygarden);
     }
