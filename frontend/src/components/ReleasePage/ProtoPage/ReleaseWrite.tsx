c9a833ce21c809f23c7924d3a51dc68900b2e07f
WChan <baewc95@gmail.com>
2023-05-12 17:31:15 +0900

    [FE]feat/writing : 이미지 유효성검사

--- frontend/src/components/ReleasePage/ProtoPage/ReleaseWrite.tsx
+++ frontend/src/components/ReleasePage/ProtoPage/ReleaseWrite.tsx
@@ -178,7 +178,7 @@ export default function ReleaseWrite() {
               onClick={() => {
                 setShowImageInput(!showImageInput);
               }}
-              className="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
+              className="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b border-gray-200 focus:ring-4 focus:ring-gray-200 hover:bg-gray-100"
             >
               <span className="flex items-center">
                 <svg
