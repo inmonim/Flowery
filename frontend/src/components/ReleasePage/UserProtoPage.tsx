a41dfdf89a61a5be982655a2856ca764865df387
이승민 <curiously12@naver.com>
2023-05-12 13:17:58 +0900

    [FE]feat/seller : 모바일 웹 안내

--- frontend/src/components/ReleasePage/UserProtoPage.tsx
+++ frontend/src/components/ReleasePage/UserProtoPage.tsx
@@ -8,11 +8,13 @@ import More from "./ProtoPage/More";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import styles from "./UserProtoPage.module.scss";
+import mobile from "../../assets/pleasemobile.png";
 
 export default function UserProtoPage() {
   const [letterData, setLetterData] = useState([]);
   const [loading, setLoading] = useState(true);
   const { messageId } = useParams() as { messageId: string };
+  const [isMobileView, setIsMobileView] = useState(false);
 
   useEffect(() => {
     console.log(messageId);
@@ -38,8 +40,30 @@ export default function UserProtoPage() {
     console.log("letterData: ", letterData);
   }, [letterData]);
 
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setIsMobileView(window.innerWidth > 500);
+    };
+
+    handleWindowResize(); // 페이지 로드 시 초기 너비에 따라 모바일 뷰 설정
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
   if (loading) {
     return <div>Loading...</div>; // 로딩 중일 때 표시할 내용
+  }
+
+  if (isMobileView) {
+    return (
+      <div className={styles.photo11}>
+        <img src={mobile} alt="Mobile" />
+      </div>
+    );
   }
 
   return (
